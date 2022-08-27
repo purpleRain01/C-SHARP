9fe69b1e6b0886a718d202ab4a599768eb5ef864
Arun <109893627+purpleRain01@users.noreply.github.com>
2022-08-27 12:02:22 +0530

    Add files via upload

---
+++ fibonacci.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fibonacciforloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 0, num2 = 1, limit,sum;
+            Console.WriteLine("enter a limit");
+            limit = Convert.ToInt32(Console.ReadLine());
+            Console.Write($"{num1},{num2},");
+            for(int i=0;i<limit-2;i++)
+            {
+                sum = num1 + num2;
+                Console.Write($"{sum},");
+                num1 = num2;
+                num2 = sum;
+            }
+        }
+    }
+}
