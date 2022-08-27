b84559e7ce93aba4ea7b6b3a462a46ee53ecd595
Arun <109893627+purpleRain01@users.noreply.github.com>
2022-08-27 11:52:13 +0530

    Add files via upload

---
+++ split_and_join_with_list.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace first
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> ls = new List<string>();
+            string str = "i.like.this.program.very.much";
+            ls = str.Split('.').ToList();
+            foreach(var a in ls)
+            {
+              Console.WriteLine(a);
+            }
+            ls.Reverse();
+            string result;
+            result=string.Join(".",ls);  
+           /* string s = "";             
+            foreach (var a in ls)      
+            {
+                s = s + a;
+                s = s + ".";
+            }*/
+            Console.WriteLine(result);
+           // s = s.Remove(s.Length - 1, 1);
+           
+           
+
+           
+            Console.ReadKey();
+        }
+    }
+}
