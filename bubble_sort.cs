9383bb6423b55db13edb1d67d1b45a3e14577e2e
Arun <109893627+purpleRain01@users.noreply.github.com>
2022-08-27 12:29:33 +0530

    Add files via upload

---
+++ bubble_sort.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bubblesort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr =new int[] { 1,3,2,10,8,4,1,6,5};
+            for (int i=0;i<arr.Length;i++)
+            {
+               
+                for(int j=0;j<arr.Length-i-1;j++)
+                {
+                    if(arr[j]>arr[j+1])
+                    {
+                        int temp = arr[j+1];
+                        arr[j+1] = arr[j];
+                        arr[j] = temp;
+                    }
+                }
+            }
+            foreach(var item in arr)
+            {
+                Console.WriteLine($"{item},");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
