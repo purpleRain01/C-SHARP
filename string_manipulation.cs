d52544d78a1f74147dcd6479da2e9f31c34c00b8
Arun <109893627+purpleRain01@users.noreply.github.com>
2022-08-27 11:24:08 +0530

    Add files via upload

---
+++ string_manipulation.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace join_concaqt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string_concat();
+            string_join();
+            Console.ReadKey();
+        }
+        public static void string_concat()
+        {
+            int[] array1 = new int[] { 1, 2, 3, 4, 5, 6, 6 };
+            string str;
+            str = string.Concat(array1);
+            Console.WriteLine("concatenated result= "+str);
+        }
+        public static void string_join()
+        {
+
+            int[] arra = new int[] { 1, 2, 3, 4, 5, 6, 7 };
+            string str = string.Join(",", arra);
+            Console.WriteLine("join method result= "+str);
+            Console.WriteLine(str.GetType());
+            string str1 = "arun,aishu,jisha,arya";
+            string[] array1 =str1.Split(',') ;
+
+
+            Array.ForEach(array1, x => Console.WriteLine(x));
+
+        }
+    }
+}
