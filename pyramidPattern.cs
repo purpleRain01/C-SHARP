bb270c080cea5f1b6293c3e3542a0ebdbdd84a4a
Arun <109893627+purpleRain01@users.noreply.github.com>
2022-09-16 16:44:37 +0530

    Add files via upload

---
+++ pyramidPattern.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _29_08
+{
+    class Program
+    {
+       public static void Main(string[] args)
+        {
+            int row;
+            Console.WriteLine("enter the rows");
+            row = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=row;i++)
+            {
+                for (int j=i; j <row; j++)
+                {
+                    Console.Write("  ");
+                }
+                for(int k=1;k<=i;k++)
+                {
+                    Console.Write(k+" ");
+                }
+                for(int l=1;l<i;l++)
+                {
+                    Console.Write(l+" ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+    }
+}
